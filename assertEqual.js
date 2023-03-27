8a30705e859fff5c7e9eac3572de662b150ffa73
John <john.cloghesy@gmail.com>
2023-03-27 00:27:11 -0600

    Implements Function - assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+// ********** assertEqual FUNCTION *********
+
+
+/** **** FUNCTION IMPLEMENTATION: assertEqual FUNCTION ****
+ *  Functionality - a function that takes in 2 values and returns true
+ *                  if they are equal and false otherwise.
+ */
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// **** TESTING: assertEqual FUNCTION **** 
+
+//     Test Set 1 Assigned Tests
+//      in Compass instructions - compare different strings & numbers
+assertEqual("Random string", "Random string");     // Rs === Rs --> true
+assertEqual("Random string ", "Different string"); // Rs !== Ds --> false
+assertEqual(5, 5);                                 //  5 === 5  --> true
+assertEqual(500, 5);                              // 500 !== 5  --> false
+
