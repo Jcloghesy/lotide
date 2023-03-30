d9a2a2aaaad10afea4bff866b8b1ef39a38a7d31
John <john.cloghesy@gmail.com>
2023-03-29 23:23:36 -0600

    FIX: return actual values not undefined - without

--- without.js
+++ without.js
@@ -25,18 +25,18 @@ const assertArraysEqual = function (myArray1, myArray2) {
   }
 };
 
-
 /** **** FUNCTION IMPLEMENTATION: without FUNCTION ****
  *  Functionality - a function that takes in a source array and 
  *                  an itemsToRemove arrays and returns only the items from 
  *                  the source array that are not in the itemsToRemove array
  *  Code    
- *   ln 1. let     - creates newArray for output
- *   ln 2. for     - outside loop loops source
- *   ln 3. let     - 
- *   ln 4. for     - inside loop 
- *   ln 5.  if     - compares source to remove
- *   ln 6.  if     - if no match then item not in remove does not add
+ *   ln  1. let     - creates newArray for output
+ *   ln  2. for     - outside loop loops source
+ *   ln  3. let     - 
+ *   ln  4. for     - inside loop 
+ *   ln  5.  if     - compares source to remove
+ *   ln  6.  if     - if no match then item not in remove does not add
+ *   ln 15. return  - returns newArray (passes to assertArraysEqual 4 Testing)
  * 
  */
 const without = function(sourceArray, itemsToRemove) {
@@ -52,22 +52,22 @@ const without = function(sourceArray, itemsToRemove) {
       newArray.push(sourceArray[i]); 
     }
   }
-  return console.log(newArray);
+  return newArray;
 };
 
 
 // **** TESTING: without FUNCTION ****
 
 //    Test Set 1: Assigned Test Cases - testing function's general results
-without([1,2,3],[1]) // => [2, 3]
-without(["1","2","3"],[1,2,"3"])// => ["1", "2"]
+assertArraysEqual(without([1,2,3],[1]), [2, 3]);
+assertArraysEqual(without(["1","2","3"],[1,2,"3"]), ["1", "2"]);
 
 //    Test Set 2: Assigned Test - ensuring original array is not modified
 const words =["hello","world","lighthouse"];
 without(words,["lighthouse"]);
-assertArraysEqual(words,["hello","world","lighthouse"]);// => true
+assertArraysEqual(words,["hello","world","lighthouse"]);
 
 //    Test Set 3: Additional Test Cases - empty arrays
-without([],[1]) // => []
-without([1],[]) // => [1]
+assertArraysEqual(without([],[1]),[]);
+assertArraysEqual(without([1],[]), [1]);
 
